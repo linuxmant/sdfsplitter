adf41367173c12afcc6a3a5eab41844666e9de17
Diego Pedrosa <linuxmant@gmail.com>
2015-02-03 07:48:23 -0800

    Initial Commit

---
+++ SDFSplitter/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SDFSplitter {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow :Window {
+
+		private Splitter splitter;
+
+		public MainWindow() {
+			InitializeComponent();
+			this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+
+			this.DataContext = splitter;
+		}
+
+		void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+			sdfFilePath.Text = "Click to select sdf file...";
+			sdfFilePath.Foreground = Brushes.Silver;
+		}
+
+		private void exitBtn_Click(object sender, RoutedEventArgs e) {
+			this.Close();
+		}
+
+		private void TextBox_MouseUp(object sender, MouseButtonEventArgs e) {
+			resultsTbx.Text = "";
+			
+			Microsoft.Win32.OpenFileDialog openDlg = new Microsoft.Win32.OpenFileDialog();
+
+			openDlg.DefaultExt = ".sdf";
+			openDlg.Filter = "SD File (.sdf)|*.sdf";
+
+			Nullable<bool> result = openDlg.ShowDialog();
+
+			if (result == true) {
+				TextBox path = (TextBox) (((FrameworkElement) sender).Parent as FrameworkElement).FindName("sdfFilePath");
+				path.Text = openDlg.FileName;
+				path.Foreground = Brushes.Black;
+				path.Focus();
+
+				molPath.Text = path.Text.Substring(0, path.Text.LastIndexOf("\\")) + "\\molFiles";
+
+				splitBtn.IsEnabled = true;
+				splitBtn.Focus();
+			}
+		}
+
+		private void splitBtn_Click(object sender, RoutedEventArgs e) {
+			splitter = new Splitter(sdfFilePath.Text, molPath.Text, resultsTbx);
+			resultsTbx.Text = "Processing...";
+			resultsTbx.BringIntoView();
+			splitter.process();
+		}
+
+		private void sdfFilePath_MouseEnter(object sender, MouseEventArgs e) {
+			if (sdfFilePath.Text.Equals("Click to select sdf file...")) {
+				sdfFilePath.Text = "";
+			}
+		}
+
+		private void sdfFilePath_MouseLeave(object sender, MouseEventArgs e) {
+			if (sdfFilePath.Text.Equals("")) {
+				sdfFilePath.Foreground = Brushes.Silver;
+				sdfFilePath.Text = "Click to select sdf file...";
+			} else {
+				sdfFilePath.Foreground = Brushes.Black;
+			}
+		}
+	}
+}
---
+++ SDFSplitter/Splitter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SDFSplitter {
+	class Splitter:INotifyPropertyChanged {
+		private String sdfFilePath;
+		private String molPath;
+		private TextBlock _results;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public TextBlock results {
+			get { return _results; }
+			set {
+				_results = value;
+
+				if (PropertyChanged != null) {
+					PropertyChanged(this, new PropertyChangedEventArgs("results"));
+				}
+			}
+		}
+
+		public Splitter(String inFile, String outPath, TextBlock monitor) {
+			this.sdfFilePath = inFile;
+			this.molPath = outPath;
+			this.results = monitor;
+		}
+
+		public bool process() {
+			bool res = false;
+			Console.WriteLine(" ...file " + sdfFilePath);
+			results.Text += "\n ...file " + sdfFilePath;
+			try {
+				if (!Directory.Exists(molPath)) {
+					Directory.CreateDirectory(molPath);
+					Console.WriteLine("Created directory " + molPath);
+				}
+
+				StringBuilder strBuild = new StringBuilder();
+				int fileCtr = 0;
+				foreach (String line in File.ReadAllLines(sdfFilePath)) {
+					if (!line.Equals("$$$$")) {
+						strBuild.AppendLine(line);
+					} else {
+						strBuild.AppendLine(line);
+						this.saveMol(("file" + fileCtr++), strBuild.ToString());
+						strBuild.Clear();
+					}
+				}
+			} catch (Exception e) {
+				Console.WriteLine("The file could not be read:");
+				Console.WriteLine(e.Message);
+				results.Text = "The file could not be read:\n";
+				results.Text += e.Message;
+			}
+
+			return res;
+		}
+
+		private void saveMol(String name, String data) {
+			name += ".mol";
+			try {
+				using(StreamWriter sw = new StreamWriter(molPath + "\\" + name)) {
+					sw.Write(data);
+					sw.Flush();
+					results.Text += "\nSaved file " + name;
+				}
+			} catch (Exception e) {
+				Console.WriteLine("The file " + name + " could not be saved:");
+				Console.WriteLine(e.Message);
+				results.Text = "The file " + name + " could not be saved:\n";
+				results.Text += e.Message;
+			}
+		}
+	}
+}
